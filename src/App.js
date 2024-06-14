2447e723837cc352417c7f37eaca7a875be00900
fanuelstephen <115701037+fanuelstephen@users.noreply.github.com>
2024-06-14 07:01:19 +0300

    adding van  and vanDetails componetns, and using useParams catching id

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Vans from './pages/Vans';
+import VansDetails from './pages/VansDetails';
 function App() {
   return (
     <div className="App">
@@ -9,11 +11,14 @@ function App() {
           <Link to="/">#VANLIFE</Link>
           <nav>
             <Link to="/about">About</Link>
+            <Link to="/vans">Vans</Link>
           </nav>
         </header>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VansDetails />} />
         </Routes>
       </BrowserRouter>
     </div>
